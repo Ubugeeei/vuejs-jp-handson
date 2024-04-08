c7afa59244787c17dca2113449ddac025474aa89
Ubugeeei <ubuge1122@gmail.com>
2024-04-09 04:04:21 +0900

    feat: cart badge

---
+++ 2024-draft/01.carts/app.vue
@@ -0,0 +1,161 @@
+<script setup lang="ts">
+const items = ref([
+  {
+    id: 1,
+    name: 'アボカドディップバケット',
+    description:
+      '刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
+    price: 480,
+    image: '/images/item1.jpg',
+    soldOut: false,
+    selected: false
+  },
+  {
+    id: 2,
+    name: 'あの日夢見たホットケーキ',
+    description:
+      '子供のころに食べたかった、あのホットケーキを再現しました。素朴でどこか懐かしい味をどうぞ。',
+    price: 1180,
+    image: '/images/item2.jpg',
+    soldOut: false,
+    selected: false
+  },
+  {
+    id: 3,
+    name: 'HOP WTR',
+    description:
+      'ロサンゼルス生まれのスパークリングウォーター。ノンカロリー、ノンアルコールの新感覚飲料です。',
+    price: 320,
+    image: '/images/item3.jpg',
+    soldOut: true,
+    selected: false
+  },
+  {
+    id: 4,
+    name: 'チーズフレンチフライ',
+    description:
+      'イタリア産チーズをたっぷりかけたアツアツのフレンチフライ。みんな大好きな一品です。',
+    price: 670,
+    image: '/images/item4.jpg',
+    soldOut: false,
+    selected: false
+  }
+])
+
+const selectedItems = computed(() => items.value.filter(item => item.selected))
+</script>
+
+<template>
+  <header class="header">
+    <img
+      src="/images/logo.svg"
+      alt="">
+    <h1>Vue.js ハンズオン</h1>
+
+    <div class="cart-icon">
+      <Icon name="mdi:cart"/>
+      <span class="cart-badge">{{ selectedItems.length }}</span>
+    </div>
+  </header>
+  <main class="main">
+    <template
+      v-for="item in items"
+      :key="item.id">
+      <div
+        v-if="!item.soldOut"
+        class="item"
+        :class="{ 'selected-item': item.selected }"
+        @click="item.selected = !item.selected">
+        <Card
+          :id="item.id"
+          :image="item.image"
+          :name="item.name"
+          :description="item.description"
+          :price="item.price">
+          <template #body>
+            <p>{{ item.description }}</p>
+          </template>
+        </Card>
+      </div>
+    </template>
+  </main>
+</template>
+
+<style>
+body {
+  font-family: sans-serif;
+  margin: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+  width: 90%;
+  margin: 0 5%;
+  text-align: center;
+  color: #242424;
+}
+
+.header {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
+.header > img {
+  width: 100px;
+  height: 100px;
+  margin-right: 20px;
+}
+
+.header > h1 {
+  font-size: 80px;
+  font-weight: bold;
+  line-height: 80px;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.main {
+  display: grid;
+  grid-template-columns: 3fr 3fr 3fr 3fr;
+  column-gap: 24px;
+  row-gap: 24px;
+}
+
+.item {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.item:hover {
+  transition: 0.2s transform ease-out;
+  transform: scale(1.05);
+}
+
+.selected-item {
+  background: #e3f2fd;
+}
+
+.cart-icon {
+  position: relative;
+  font-size: 32px;
+}
+
+.cart-badge {
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  background: red;
+  color: white;
+  border-radius: 50%;
+  padding: 5px;
+  width: 16px;
+  height: 16px;
+  font-size: 12px;
+  line-height: 16px;
+  text-align: center;
+}
+</style>
