4b591b72512fec9b375760f3daba835929c6ef7e
Ubugeeei <ubuge1122@gmail.com>
2024-04-09 04:46:11 +0900

    feat: layout

---
+++ 2024-draft/03.layouts/layouts/default.vue
@@ -0,0 +1,72 @@
+<template>
+  <header class="header">
+    <div class="header-title">
+      <img class="vuejs-jp-logo" src="/images/logo.svg" alt="">
+      <h1>Vue.js ハンズオン</h1>
+    </div>
+
+    <NuxtLink to="/carts" style="text-decoration: none; color: inherit">
+      <div class="cart-icon">
+        <Icon name="mdi:cart"/>
+        <!-- TODO: -->
+        <!-- <span class="cart-badge">{{ selectedItems.length }}</span> -->
+      </div>
+    </NuxtLink>
+  </header>
+
+  <main class="page-main">
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+.header {
+  display: grid;
+  grid-template-columns: 1fr 60px;
+  padding: 16px;
+}
+
+.header > .header-title {
+  display: grid;
+  grid-template-columns: 60px auto;
+  grid-auto-flow: column;
+}
+
+.header-title > img {
+  align-self: center;
+  width: 32px;
+  height: 32px;
+  margin-right: 20px;
+}
+
+.header-title > h1 {
+  font-size: 32px;
+  font-weight: bold;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.cart-icon {
+  position: relative;
+  font-size: 32px;
+}
+
+.cart-badge {
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  background: red;
+  color: white;
+  border-radius: 50%;
+  padding: 5px;
+  width: 16px;
+  height: 16px;
+  font-size: 12px;
+  line-height: 16px;
+  text-align: center;
+}
+
+.page-main {
+  padding: 16px;
+}
+</style>
---
+++ 2024-draft/03.layouts/pages/index.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+const items = ref([
+  {
+    id: 1,
+    name: 'アボカドディップバケット',
+    description:
+      '刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
+    price: 480,
+    image: '/images/item1.jpg',
+    soldOut: false,
+    selected: false
+  },
+  {
+    id: 2,
+    name: 'あの日夢見たホットケーキ',
+    description:
+      '子供のころに食べたかった、あのホットケーキを再現しました。素朴でどこか懐かしい味をどうぞ。',
+    price: 1180,
+    image: '/images/item2.jpg',
+    soldOut: false,
+    selected: false
+  },
+  {
+    id: 3,
+    name: 'HOP WTR',
+    description:
+      'ロサンゼルス生まれのスパークリングウォーター。ノンカロリー、ノンアルコールの新感覚飲料です。',
+    price: 320,
+    image: '/images/item3.jpg',
+    soldOut: true,
+    selected: false
+  },
+  {
+    id: 4,
+    name: 'チーズフレンチフライ',
+    description:
+      'イタリア産チーズをたっぷりかけたアツアツのフレンチフライ。みんな大好きな一品です。',
+    price: 670,
+    image: '/images/item4.jpg',
+    soldOut: false,
+    selected: false
+  }
+])
+</script>
+
+<template>
+  <ul class="item-list">
+    <li v-for="item in items.filter(item => !item.soldOut)" :key="item.id" class="item">
+      <Card
+        :class="{ 'selected-item': item.selected }"
+        :id="item.id"
+        :image="item.image"
+        :name="item.name"
+        :description="item.description"
+        :price="item.price"
+        @click="item.selected = !item.selected">
+      >
+        <template #body>
+          <p>{{ item.description }}</p>
+        </template>
+      </Card>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.item-list {
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  column-gap: 24px;
+
+  /* reset */
+  list-style: none;
+  padding: 0;
+}
+
+.item {
+  display: grid;
+}
+
+.item:hover {
+  transition: 0.2s transform ease-out;
+  transform: scale(1.05);
+}
+
+.selected-item {
+  background: #e3f2fd;
+}
+</style>
